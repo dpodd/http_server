44a5f0aee8ac6279a1976ad20d372e12610abda3
dpodd <fuzzyworld15@gmail.com>
2021-01-29 15:51:16 +0700

    (save 29/01 - 2)

---
+++ htmlgen.py
@@ -0,0 +1,3 @@
+# TODO: implement generating INDEX html with "tree" bash command
+# https://stackoverflow.com/questions/3785055/how-can-i-create-a-simple-index-html-file-which-lists-all-files-directories
+# with falling back to simple ls method with a template
--- httpd.py
+++ httpd.py
@@ -1,62 +1,123 @@
 import socket
+import os
 from optparse import OptionParser
 from icecream import ic
-from threading import Lock
 from concurrent.futures import ThreadPoolExecutor
 import logging
-from connection import Connection
+
+CRLFx2 = b'\r\n\r\n'
 
 
-def parse_headers(raw_headers):
-    headers = raw_headers.split(b'\r\n')
-    ic("in parse_headers", headers)
-    return headers
+class RequestHandler:
+    @classmethod
+    def get_response(cls, request, _dir):
+        response = cls(request, _dir)
+        response.process_request()
+        return response.to_binary() # TODO ??? Refactor?
+
+    def __init__(self, request, _dir):
+        self.request = request
+        self.dir = _dir
+
+    def process_request(self):
+        # 1 process resourse
+        path_to_file = self.get_path(self.request.path)
+        # 2 process METHOD GET/HEAD
+
+    def get_path(self, path):
+
+
+    def to_binary(self):
+        pass
 
 
 class Request:
     def __init__(self, raw_request):
         self.raw = raw_request
+        self.lines = [item.decode() for item in self.raw.split(b'\r\n') if item]
+        self.headers = self.get_headers()
+        self.method = self.get_method()
+        self.path = self.get_path()
 
-    @property
-    def headers(self):
-        return {h[:h.find(b':')].decode(): h[h.find(b':'):].decode() for h in self.parse_headers()}
+    def get_headers(self):
+        lines = self.lines[1:]  # throw away the start line
+        return {l[:l.find(':')]: l[l.find(':')+1:].strip() for l in lines}
 
-    def parse_headers(self):
-        return self.raw.split(b'\r\n')[1:]
+    def get_method(self):
+        return self.lines[0].split()[0]
+
+    def get_path(self):
+        return self.lines[0].split()[1]
 
 
+class Worker:
+    def __call__(self, conn, _dir):
+        self.process(conn, _dir)
 
-def http_handler(conn):
-    CRLFx2 = b'\r\n\r\n'
-    with conn:
+    def process(self, conn, _dir):
+        with conn:
+            while True:
+                try:
+                    ic('Waiting for new request')
+                    buff = conn.recv(1024) # TODO move to consts
+                    print(buff)
+                    if not buff: break  # if client closed socket
+                    if CRLFx2 not in buff:
+                        # send 500 invalid request?  # TODO refactor this code, make standard implementation
+                        raise NotImplementedError("Unsupported request: no CRLF delimiter")
+
+                    pos = buff.find(CRLFx2) + len(CRLFx2) # TODO Remove?
+                    raw_req = buff[:pos]  # server throws out the body of the request if it exists
+
+                    request = Request(raw_req)
+                    ic(request.headers)
+                    ic(request.method)
+                    # try:
+                    #     response = RequestHandler.get_response(request, _dir)
+                    # except:
+                    #     pass
+
+                    # conn.sendall(response)
+
+                except NotImplementedError as e:
+                    logging.exception(f"An error occurred; the message is: {e}")
+                    break
+                except socket.timeout:
+                    logging.exception('Timeout error. Closing the socket...')
+                    break
+
+
+class Server:
+    def __init__(self, host, port, max_workers, root_directory):
+        self.socket = None
+        self.host = host
+        self.port = port
+        self.max_workers = max_workers
+        self.root_directory = root_directory
+        self._check_root_dir()
+
+    def _check_root_dir(self):
+        _dir = os.path.abspath('.')
+        _dir = os.path.join(_dir, self.root_directory)
+        if not os.path.exists(_dir):
+            raise NotADirectoryError(f"Path not found: {_dir}")
+
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen()
+
+        logging.info("Server is ready to accept connections...")
         while True:
             try:
-                ic('Waiting for new request')
-                buff = conn.recv(1024)
-                print(buff)
-                if not buff: break  # client closed socket
-                if CRLFx2 not in buff:
-                    raise NotImplementedError("Invalid request: no CRLF delimiter")
-
-                pos = buff.find(CRLFx2)
-                raw_req = buff[:pos]  # server assumes the request has no body
-
-                ic('hello')
-                request = Request(raw_req)
-                ic(request.headers)
-                # conn.sendall(b'HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n')
-                # conn.close() # closing socket, implementing short-lived connection server
-                # conn.sendall(b'')
-
-                # raw_req = headers
-
-
-            except NotImplementedError as e:
-                logging.exception(f"An error occurred with this message: {e}")
-                break
-            except socket.timeout:
-                logging.info('Timeout error. Closing the socket...')
-                break
+                conn, addr = self.socket.accept()
+                conn.settimeout(10) # TODO ADD TO CONSTS
+                with ThreadPoolExecutor(max_workers=5) as executor: # TODO max_workers
+                    executor.submit(Worker(), conn, self.root_directory)
+            except Exception as e:
+                logging.info("Error during handling request. Details: %s" % e)
+            break  # TODO : delete after debug
 
 
 if __name__ == '__main__':
@@ -64,31 +125,21 @@ if __name__ == '__main__':
     op.add_option("-p", "--port", action="store", type=int, default=40000)
     op.add_option("--host", action="store", type=str, default='localhost')
     op.add_option("-w", "--workers", action="store", type=int, default=5)
+    op.add_option("-r", "--root_dir", action="store", type=str, default='/') # TODO turn to Arg? HARDCODED!
     opts, args = op.parse_args()
 
     logging.basicConfig(level=logging.INFO)
     logging.info(f"Server is running on host '{opts.host}' and port {opts.port}")
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((opts.host, opts.port))
-    s.listen(5)
+    try:
+        server = Server(opts.host, opts.port, opts.workers, opts.root_dir)
+    except NotADirectoryError as e:
+        logging.error("Cannot start server: %s" % e)
+        raise
 
-    stdout_lock = Lock()
-
-    while True:
-        try:
-            logging.info("Server is ready to accept connection...")
-            conn, addr = s.accept()
-            conn.settimeout(10)
-            #connection = Connection(conn)
-            with ThreadPoolExecutor(max_workers=5) as executor:
-                executor.submit(http_handler, conn)
-                #t = Thread(target=http_handler, args=(conn, ))
-
-        except Exception as e:
-            logging.info("Error! Details: %s" % e)
-        break
-
-    logging.info("closing server....")
-    s.close()
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        logging.info("Server has been stopped by admin")
+    except Exception as e:
+        logging.error("Server has been crashed %s" % e) # TODO add msg
