ee9aed4653793a6aeee9936bc99771f2e6e75dc2
dpodd <fuzzyworld15@gmail.com>
2021-01-29 07:17:30 +0700

    (save 29.01)

--- httpd.py
+++ httpd.py
@@ -7,48 +7,53 @@ import logging
 from connection import Connection
 
 
-def http_handler(conn, addr):
-    with stdout_lock:
-        print(f"New connection with {addr}")
+def parse_headers(raw_headers):
+    headers = raw_headers.split(b'\r\n')
+    ic("in parse_headers", headers)
+    return headers
+
+
+class Request:
+    def __init__(self, raw_request):
+        self.raw = raw_request
+
+    @property
+    def headers(self):
+        return {h[:h.find(b':')].decode(): h[h.find(b':'):].decode() for h in self.parse_headers()}
+
+    def parse_headers(self):
+        return self.raw.split(b'\r\n')[1:]
+
+
+
+def http_handler(conn):
     CRLFx2 = b'\r\n\r\n'
     with conn:
-        buff = b''
-        old_buffer_remainder = b''
-        raw_req = b''
         while True:
             try:
                 ic('Waiting for new request')
                 buff = conn.recv(1024)
-                buff = old_buffer_remainder + buff
                 print(buff)
-                print('LENGTH', len(buff))
-                if not buff: break # client closed socket
+                if not buff: break  # client closed socket
                 if CRLFx2 not in buff:
-                    raw_req += buff # for long requests - adding data to the request. not tested
-                    buff = b''
-                while buff:
-                    if CRLFx2 in buff:
-                        pos = buff.find(CRLFx2)
-                        msg_len = pos + len(CRLFx2)
-                        raw_headers += buff[:msg_len] # case 1: no content-length
-                        # TODO: send request to requests handler Request(req)
-                        request =
-                        # now parse headers and find Content-Length header
-                        parse_headers()
-                        # conn.sendall(b'HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n')
+                    raise NotImplementedError("Invalid request: no CRLF delimiter")
 
-                        # conn.close() # closing socket, implementing short-lived connection server
-                        # conn.sendall(b'')
-                        print("REQUEST END FOUND. Request: ", req, sep='\n')
-                        print(buff)
-                        buff = buff[msg_len:]
-                        ic("new buffer:", buff)
-                    else:
-                        old_buffer_remainder = buff
-                        buff = b''
+                pos = buff.find(CRLFx2)
+                raw_req = buff[:pos]  # server assumes the request has no body
 
-            # except RuntimeError:
-            #     break
+                ic('hello')
+                request = Request(raw_req)
+                ic(request.headers)
+                # conn.sendall(b'HTTP/1.1 200 OK\r\nConnection: close\r\n\r\n')
+                # conn.close() # closing socket, implementing short-lived connection server
+                # conn.sendall(b'')
+
+                # raw_req = headers
+
+
+            except NotImplementedError as e:
+                logging.exception(f"An error occurred with this message: {e}")
+                break
             except socket.timeout:
                 logging.info('Timeout error. Closing the socket...')
                 break
@@ -61,7 +66,8 @@ if __name__ == '__main__':
     op.add_option("-w", "--workers", action="store", type=int, default=5)
     opts, args = op.parse_args()
 
-    print(f"Server is running on host '{opts.host}' and port {opts.port}")
+    logging.basicConfig(level=logging.INFO)
+    logging.info(f"Server is running on host '{opts.host}' and port {opts.port}")
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -72,17 +78,17 @@ if __name__ == '__main__':
 
     while True:
         try:
-            print("Server is ready to accept connection...")
+            logging.info("Server is ready to accept connection...")
             conn, addr = s.accept()
             conn.settimeout(10)
             #connection = Connection(conn)
             with ThreadPoolExecutor(max_workers=5) as executor:
-                executor.submit(http_handler, conn, addr)
-                #t = Thread(target=http_handler, args=(conn, addr))
+                executor.submit(http_handler, conn)
+                #t = Thread(target=http_handler, args=(conn, ))
 
         except Exception as e:
-            print("Error! Details: %s" % e)
+            logging.info("Error! Details: %s" % e)
         break
 
-    print("closing server....")
+    logging.info("closing server....")
     s.close()
